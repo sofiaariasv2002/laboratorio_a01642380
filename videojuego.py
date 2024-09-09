936cd80a5ca542a1024c3b21d0144e57868f7bba
root <root@MSI-de-Sofia>
2024-09-09 10:53:51 -0600

    draw a rectangle

--- videojuego.py
+++ videojuego.py
@@ -39,11 +39,23 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count%2== 0:
+            forward(end.x - start.x)
+            left(90)
+        else:
+            forward(end.x+30 - start.x+30)
+            left(90)
+    end_fill()
 
 def triangle(start, end):
     "Draw triangle from start to end."
-    pass  # TODO
+    pass: #TODO
 
 def tap(x, y):
     "Store starting point or draw shape."
