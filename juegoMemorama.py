ea3ae6cbacc8100df93ce7905c44462963dd890a
sofiaariasv2002 <sofiaariasv2002@gmail.com>
2024-09-10 00:10:06 -0600

    center digits in the function draw

--- juegoMemorama.py
+++ juegoMemorama.py
@@ -56,9 +56,9 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x +27, y-3)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], align="center", font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
